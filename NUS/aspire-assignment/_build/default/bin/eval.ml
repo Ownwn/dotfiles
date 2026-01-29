51c7b9776257b19ce56395fb63c7f8af8ab122ac
Owen Suter <owen@ownwn.com>
2026-01-29 13:45:25 +0800

    start on parse

--- NUS/aspire-assignment/_build/default/bin/eval.ml
+++ NUS/aspire-assignment/_build/default/bin/eval.ml
@@ -5,27 +5,6 @@ open Aspire.Value
 other exceptions should be raised. *)
 exception EvalExn of string
 
-let env_update (name: Ident.t) (value: Value.t) (env: env): env =
-  ConsEnv (name, value, env)
-
-let rec env_lookup (name: Ident.t) (env: env): Value.t option =
-  match env with
-  | EmptyEnv -> None
-  | ConsEnv (k, v, rest) -> if (k = name) then Some v else env_lookup name rest
-  
-(* Evaluate a binop applied to two floats.  May raise EvalExn on division by
-zero.
-
-Hint: For compaing a float with zero, use `=`. *)
-let eval_binop (op: Untyped.binop) (x: float) (y: float): float =
-  match op with
-  | Add -> x +. y
-  | Mul -> x *. y
-  | Div -> (if (not (y = 0.)) then x /. y else raise (EvalExn "Division by zero"))
-  | Sub -> x -. y
-
-  (* dune exec -- aspire test owen-tests --detail --ref=all *)
-
 let rec length(l: 'a list): int = 
   match l with 
   | [] -> 0
@@ -42,26 +21,37 @@ let rec find_duplicate (l: 'a list): 'a option =
   | [] -> None
   | head :: tail -> if (contains head tail) then (Some head) else (find_duplicate tail)
 
-
-let get_float (t: t): float = 
-  match t with 
-  | Float f -> f
-  | _ -> failwith "Expected float when evaluating expr!"
-  
-
-let rec debug_print_list(l: 'a list) (stringify: 'a -> string): unit = 
-  match l with 
-  | [] -> ()
-  | head :: tail -> 
-    let str = (stringify head) in 
-     (print_endline str); debug_print_list tail stringify
-
-
 (* this is FIFO, can swap the reduce and accumulator call order to reverse it *)
 let rec reduce (identity: 'a) (accumulator: 'a -> 'b -> 'a) (list: 'b list): 'a = 
   match list with
   | [] -> identity
   | (head: 'b) :: (tail: 'b list) -> reduce (accumulator identity head) accumulator tail
+
+
+(* FIFO *)
+let rec zip (l1: 'a list) (l2: 'b list) (zipper: 'a -> 'b -> 'c): 'c list =
+    let fail = fun () -> raise ( EvalExn "List sizes differ in zip call :(") in (* Should never be raised unless the eval impl gets messed up*)
+    match l1 with 
+    | [] -> (match l2 with | [] -> [] | _ :: _ -> fail ())
+    | head :: tail -> (
+      match l2 with 
+      | [] -> fail ()
+      | head2 :: tail2 -> ((zipper head head2) :: (zip tail tail2 zipper))
+    )
+
+(* FIFO *)
+let rec map(l: 'a list) (mapper: 'a -> 'b): 'b list = 
+  match l with 
+  | [] -> []
+  | head :: tail -> (mapper head) :: map tail mapper
+
+let env_update (name: Ident.t) (value: Value.t) (env: env): env =
+  ConsEnv (name, value, env)
+
+let rec env_lookup (name: Ident.t) (env: env): Value.t option =
+  match env with
+  | EmptyEnv -> None
+  | ConsEnv (k, v, rest) -> if (k = name) then Some v else env_lookup name rest
 
 
 (* Adds all the variables in new_var_list to old_env (evaluates them first), they should shadow/override any old definitions *)
@@ -79,22 +69,28 @@ let add_values_to_env (old_env: env) (new_var_list: (Ident.t * Value.t) list): e
       in 
       (reduce old_env (accumulator) new_var_list) 
 
+  
+(* Evaluate a binop applied to two floats.  May raise EvalExn on division by
+zero.
+
+Hint: For compaing a float with zero, use `=`. *)
+let eval_binop (op: Untyped.binop) (x: float) (y: float): float =
+  match op with
+  | Add -> x +. y
+  | Mul -> x *. y
+  | Div -> (if (not (y = 0.)) then x /. y else raise (EvalExn "Division by zero"))
+  | Sub -> x -. y
+
+  (* dune exec -- aspire test owen-tests --detail --ref=all *)
 
 
-let rec zip (l1: 'a list) (l2: 'b list) (zipper: 'a -> 'b -> 'c): 'c list =
-    let fail = fun () -> (failwith "List sizes differ in zip call :(") in
-    match l1 with 
-    | [] -> (match l2 with | [] -> [] | _ :: _ -> fail ())
-    | head :: tail -> (
-      match l2 with 
-      | [] -> fail ()
-      | head2 :: tail2 -> ((zipper head head2) :: (zip tail tail2 zipper))
-    )
-
-let rec map(l: 'a list) (mapper: 'a -> 'b): 'b list = 
+  (* Used for debugging closure call errors with the wrong number of args *)
+let rec debug_print_list(l: 'a list) (stringify: 'a -> string): unit = 
   match l with 
-  | [] -> []
-  | head :: tail -> (mapper head) :: map tail mapper
+  | [] -> ()
+  | head :: tail -> 
+    let str = (stringify head) in 
+     (print_endline str); debug_print_list tail stringify
 
 
 (* Evaluate an expression in a given environment.  EvalExn on errors. *)
@@ -114,8 +110,8 @@ let rec eval_expr (e: Untyped.expr) (env: env): Value.t =
     let cond_body = eval_expr con env in 
     let cond_res = (match cond_body with 
     | Float f -> f
-    | Closure (cap, body, closure_env) -> let foo = (Closure (cap, body, closure_env)) in 
-    ((raise (EvalExn ("Non-number in if0: " ^ Value.show foo))))) in
+    | Closure (cap, body, closure_env) -> let closure_wrong_place = (Closure (cap, body, closure_env)) in 
+    ((raise (EvalExn ("Non-number in if0: " ^ Value.show closure_wrong_place))))) in
     if (cond_res = 0.) then eval_expr yes_b env else eval_expr no_b env
   )
 
@@ -151,23 +147,12 @@ let rec eval_expr (e: Untyped.expr) (env: env): Value.t =
       if (args_length <> params_length) then 
         (raise (EvalExn ("Function expected " ^ (string_of_int params_length) ^ " arguments but got " ^ (string_of_int args_length)))) 
       else (
-        (* (args_length |> string_of_int |> print_endline);
-        (params_length |> string_of_int |> print_endline);  *)
-        (* (debug_print_list params (fun head -> let (Ident s) = head in s));
-        (debug_print_list args (fun a -> (match eval_expr a env with 
-        
-        | Float f -> (f |> string_of_float)
-        | Closure (ll,mm,nn) -> ("nasty closure")
-        
-        ))); *)
         let evaled_args: Value.t list = (map args (fun arg_p -> eval_expr arg_p env)) in
         let zipped_args = (zip evaled_args params (fun arg_p -> fun param_p -> ( (param_p, arg_p)))) in
         let final_call_env = (add_values_to_env closure_env zipped_args) in
         (eval_expr body final_call_env)
 
       )
-
-
     )
     | _ -> (raise (EvalExn "Attempting to call a non-function value") )
   )
--- NUS/aspire-assignment/_build/default/bin/parse.ml
+++ NUS/aspire-assignment/_build/default/bin/parse.ml
@@ -3,12 +3,28 @@ open Aspire
 open Aspire.Surface
 
 
+let rec contains (x: 'a) (l: 'a list): bool =
+  match l with
+  | [] -> false
+  | head :: tail -> (head = x) || contains x tail
+
+let rec find_duplicate (l: 'a list): 'a option = 
+  match l with
+  | [] -> None
+  | head :: tail -> if (contains head tail) then (Some head) else (find_duplicate tail)
+
+
 (* NOTE: Some of these functions may raise a ParseExn.  No other exceptions
 should be raised. *)
 
 (* Convert an operator name to a `binop` *)
-let parse_binop (s: string) : binop =
-  failwith (__LOC__ ^ ": unimplemented")
+let parse_binop (s: string) : binop option = (* todo I have modified this to be option, previously it wasnt option. *)
+  match s with 
+  | "+" -> Some Add
+  | "-" -> Some Sub
+  | "*" -> Some Mul
+  | "/" -> Some Div
+  | _ -> None
 
 let make_ident (s: string) : Ident.t option =
   match float_of_string_opt s with
@@ -19,11 +35,9 @@ let make_ident (s: string) : Ident.t option =
     else Some (Ident.Ident s)
 
 
-(* Raise a ParseExn if any of the idneitifiers are duplicates of each other.
-
-Hint: Use List.exists and recursion *)
-let rec check_identifiers (bindings: Ident.t list) : unit =
-  failwith (__LOC__ ^ ": unimplemented")
+let rec check_identifiers (bindings: Ident.t list) : unit = (* todo need to use this actually!*)
+  match find_duplicate bindings with | None -> () | _ -> (* todo can i use if expr instead of match to grab Option? *)
+    failwith "todo error name here"
 
 
 (* Parse an s-expression into an expr.
@@ -32,18 +46,45 @@ NOTE: use float_of_string_opt to distinguish an Atom that should be a float from
 one that should be an identifier, then use make_ident to make identifiers *)
 let rec parse_expr (sexp: Sexp.t) : expr =
   match sexp with 
-  | Atom a -> (Float (float_of_string a))
-  | List (Atom "+" :: tail) -> (
-    match tail with 
-    | [op1; op2] -> (BinOp (Add, parse_expr op1, parse_expr op2))
-    | _ -> failwith "Bad Add operand"
-  ) 
-  | List (Atom "if0" :: tail) -> (
-    match tail with 
-    | [con; yes_b; no_b] -> (If0 (parse_expr con, parse_expr yes_b, parse_expr no_b))
-    | _ -> failwith "Bad If0 operand"
+  | Atom a -> (
+    match float_of_string_opt a with 
+    | Some f -> Float f
+    | None -> (
+      match make_ident a with 
+      | Some ident -> Var ident
+      | None -> failwith "todo checkme here aa"
+    )
   )
+
+  | List (Atom head :: tail) -> (
+    match parse_binop head with 
+    | Some bin -> (
+      match tail with 
+      | [op1; op2] -> (BinOp (bin, parse_expr op1, parse_expr op2))
+      | _ -> (failwith "tood bad num of operands")
+    )
+    | None -> (
+      match head with 
+      | "if0" -> (
+        match tail with 
+        | [con; yes_b; no_b] -> (If0 (parse_expr con, parse_expr yes_b, parse_expr no_b))
+        | _ -> failwith "Bad If0 operand"
+      )
+
+      | "let" ->  (failwith "okokokokokokk")
+
+      | _ -> (failwith "other bad parse todo")
+
+    
+    )
+  )
+
   | _ -> failwith "parse NYI"
+
+  
+
+  
+  
 
 
 (* Parse an expression representing a binding  *)
